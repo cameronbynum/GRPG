e5ff595f4427f875ebd65b43744fb649d1ec7e6f
Cameron Bynum <bynum.cameron@gmail.com>
2021-05-21 21:16:45 -0400

    updates folder directory

--- Google Earth RPG/javascript/map-options.js
+++ Google Earth RPG/javascript/map-options.js
@@ -6,7 +6,13 @@
             lat: 37.791350,
             lng: -122.435883
         },
-        zoom: 10
+        zoom: 10,
+        disableDefaultUI: true,
+        scrollwheel: true,
+        draggable: true,
+        //roadmap, satellite, hybrid, terrain
+        mapTypeId: google.maps.MapTypeId.roadmap
+
     }
     element = document.getElementById('map-canvas')
     //map
